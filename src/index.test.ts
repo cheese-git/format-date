a38e81cdd6896fe3a7d583790ed961ebeb414c89
cheese <cheese-h@qq.com>
2021-01-13 23:03:20 +0800

    add test

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,4 +3,5 @@ import formatDate from './index'
 test('format date', () => {
     const date = new Date('2021-1-13 9:08:43')
     expect(formatDate(date, 'YYYY-M-D WWW hh:mm:ss')).toBe('2021-1-13 星期三 09:08:43')
+    expect(formatDate(date, 'YYYY-MM-DD WW h:m:s')).toBe('2021-01-13 周三 9:8:43')
 })
